fb96f92592b926ea9aa85c5672db5ad67070f7a5
milin3t <minjunseong@naver.com>
2025-12-30 19:33:04 +0900

    📝 docs : AuthPage 주석 추가

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+/**
+ * DO NOT REMOVE
+ * 로그인 페이지 레이아웃을 구성하는 엔트리 컴포넌트
+ * 좌측 소개 패널과 우측 로그인 패널을 조합합니다.
+ */
+
 import AuthPageLeftSide from "@/pages/auth/ui/AuthPageLeftSide";
 import AuthPageRightSide from "@/pages/auth/ui/AuthPageRightSide";
 
