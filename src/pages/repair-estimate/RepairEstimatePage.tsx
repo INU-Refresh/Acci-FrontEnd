e55f6b4d5f974466b916a98bed41e53a1487a5f8
LimJaeHyun <guswogus81@gmail.com>
2026-01-03 22:54:37 +0900

    ✨ feat : 수리비견적 페이지 생성

--- src/pages/repair-estimate/RepairEstimatePage.tsx
+++ src/pages/repair-estimate/RepairEstimatePage.tsx
@@ -1,11 +1,14 @@
+import { Footer } from "@/widgets/footer/Footer";
+import { Header } from "@/widgets/header/Header";
+
 export default function RepairEstimatePage() {
   return (
-    <div className="min-h-screen bg-white">
-      <div className="container mx-auto px-3 sm:px-4 md:px-6 lg:px-8 max-w-7xl py-8 sm:py-12 md:py-16">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900 mb-6">수리비견적</h1>
-        <p className="text-gray-600">수리비견적 페이지입니다.</p>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Header/>
+        <section className="container mx-auto px-4 sm:px-10 py-10 sm:py-20 max-w-7xl">
+        </section>
+        <Footer/>
       </div>
-    </div>
   );
 }
 
