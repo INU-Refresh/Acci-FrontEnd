b045c86bc23029ed5c97a2892c405c47417cf7b7
milin3t <minjunseong@naver.com>
2026-02-19 16:28:15 +0900

    ♻️ refactor : 서버 사용자 정보 주입 헤더 적용 누락 페이지 반영

--- src/pages/analyze/upload/UploadPage.tsx
+++ src/pages/analyze/upload/UploadPage.tsx
@@ -1,11 +1,11 @@
 import { AnalyzeUploadSection } from "@/widgets/analyze-upload/AnalyzeUploadSection";
 import { Footer } from "@/widgets/footer/Footer";
-import { Header } from "@/widgets/header/Header";
+import HeaderWithUserInfo from "@/widgets/header/HeaderWithUserInfo";
 
 export default function UploadPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
+      <HeaderWithUserInfo />
       <main className="flex w-full flex-1 justify-center px-4 py-6 md:px-0 md:py-10">
         <AnalyzeUploadSection />
       </main>
