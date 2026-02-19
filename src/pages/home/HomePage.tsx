07a74db74a0d32f29ce5654d8bdaa175474073d3
milin3t <minjunseong@naver.com>
2026-02-19 16:28:15 +0900

    ♻️ refactor : 서버 사용자 정보 주입 헤더 적용 누락 페이지 반영

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@/widgets/header/Header";
+import HeaderWithUserInfo from "@/widgets/header/HeaderWithUserInfo";
 import { Footer } from "@/widgets/footer/Footer";
 import { HeroSection, FeaturesSection, EstimateSection, ReviewsSection, CtaSection } from "@/widgets/home";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
+      <HeaderWithUserInfo />
       <HeroSection />
       <FeaturesSection />
       <EstimateSection />
